ef867515a687b29ffe084708a1fa21f97d95f8e1
K_hitoshi <kubotah1115@gmail.com>
2023-04-21 04:23:55 +0900

    main内容変更 grpcurlの導入途中

---
+++ cmd/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	//TODOふぃあるのパス確認
+	hellopb "./pkg/grpc"
+
+	"google.golang.org/grpc"
+)
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+// サービスメソッド
+func (m *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("hello %s", req.GetName()),
+	}, nil
+}
+
+// 自作サービス構造体のコンストラクタを定義
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func main() {
+	//ポート番号8080のLisnterを作成
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	//gRPCserverを作成
+	s := grpc.NewServer()
+
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	//作成したgRPCserverを稼働させる
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	//Ctrl+Cが入力されたらGraceful shutdownされるようにする
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
--- server/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/Khitoshi/gRPCtutorial
-
-go 1.20
-
-require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	google.golang.org/grpc v1.54.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
-)
